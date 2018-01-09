2b2c18cf6cc1e766d31cc5c43dcfe77ea7d77388
RichGraham <Richard.Graham@nottingham.ac.uk>
2018-01-09 10:23:46 +0000

    Can now load more than one PES at a time

--- Example.c
+++ Example.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-extern double pes_(double *a);
-//extern void load_gp_2ar_data_(void);
-//extern double pes_2ar_(double *xStar);
+extern void load_gp_2ar_data_(void);
+extern double pes_2ar_(double *xStar);
 
 extern void load_gp_co2_ar_data_(void);
 extern double pes_co2_ar_(double *xStar);
@@ -17,17 +16,14 @@ int main()
   double lCO = 1.1632;
   
   load_gp_co2_ar_data_();
-  //load_gp_2ar_data_();
+  load_gp_2ar_data_();
 
   for(i = 1  ;   i <= itot    ;   i++){
     r = (  0.5 + 15.0*i/(1.0*itot) );
     rab[0]= r;
     rab[1]= sqrt( pow(lCO*sin(beta1),2) +pow(lCO*cos(beta1)-r,2)  );
     rab[2]= sqrt( pow(lCO*sin(beta1),2) +pow(lCO*cos(beta1)+r,2)  );
-    printf("%lf %le\n",r, pes_co2_ar_(rab));
+    printf("%lf %le\n",r, pes_co2_ar_(rab), pes_2ar_(&r));
   }
-
-  r = 2.89   *   1.8897259885789;
-  //printf("%lf\n",pes_2ar_(&a));
 
 }
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-VPATH = ./ : ../CO2-Ar_PES # : ../2Ar_PES
+VPATH = ./ : ../CO2-Ar_PES  : ../2Ar_PES
 
 FCFLAGS = -O3
 F90= gfortran
@@ -8,7 +8,7 @@ CC= gcc
 
 LIBS_PATH = -L/usr/local/Cellar/gcc/5.1.0/lib/gcc/5
 
-SOURCES= Example.c CO2-Ar_PES_GP_Symm.f90 # 2Ar_PES_GP_Symm.f90
+SOURCES= Example.c CO2-Ar_PES_GP_Symm.f90  2Ar_PES_GP_Symm.f90
 OBJECTS = $(addsuffix .o,$(basename $(SOURCES)))
 PRODUCT= merged.out
 
