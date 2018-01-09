80e78600e2ef970e4aa5d6b62a366cb8d4873975
RichGraham <Richard.Graham@nottingham.ac.uk>
2018-01-09 09:38:29 +0000

    Can run CO2-Ar_PES

--- Example.c
+++ Example.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-//extern double pes_(double *a);
-extern void load_gp_2ar_data_(void);
-extern double pes_2ar_(double *xStar);
+#include <math.h>
+extern double pes_(double *a);
+//extern void load_gp_2ar_data_(void);
+//extern double pes_2ar_(double *xStar);
+
+extern void load_gp_co2_ar_data_(void);
+extern double pes_co2_ar_(double *xStar);
+
 
 int main()
 {
-  double a,z;
+  int i,itot=500;
+  double r,z, beta1=0;
+  double rab[3]={0};
+  double lCO = 1.1632;
   
-  a = 2.89   *   1.8897259885789;
-  load_gp_2ar_data_();
-  printf("%lf\n",pes_2ar_(&a));
+  load_gp_co2_ar_data_();
+  //load_gp_2ar_data_();
+
+  for(i = 1  ;   i <= itot    ;   i++){
+    r = (  0.5 + 15.0*i/(1.0*itot) );
+    rab[0]= r;
+    rab[1]= sqrt( pow(lCO*sin(beta1),2) +pow(lCO*cos(beta1)-r,2)  );
+    rab[2]= sqrt( pow(lCO*sin(beta1),2) +pow(lCO*cos(beta1)+r,2)  );
+    printf("%lf %le\n",r, pes_co2_ar_(rab));
+  }
+
+  r = 2.89   *   1.8897259885789;
+  //printf("%lf\n",pes_2ar_(&a));
+
 }
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-VPATH = ./ : ../2Ar_PES # : ../CO2-Ar_PES
+VPATH = ./ : ../CO2-Ar_PES # : ../2Ar_PES
 
 FCFLAGS = -O3
 F90= gfortran
@@ -8,13 +8,13 @@ CC= gcc
 
 LIBS_PATH = -L/usr/local/Cellar/gcc/5.1.0/lib/gcc/5
 
-SOURCES= Example.c   2Ar_PES_GP_Symm.f90 #CO2-Ar_PES_GP_Symm.f90
+SOURCES= Example.c CO2-Ar_PES_GP_Symm.f90 # 2Ar_PES_GP_Symm.f90
 OBJECTS = $(addsuffix .o,$(basename $(SOURCES)))
 PRODUCT= merged.out
 
 
 $(PRODUCT): $(OBJECTS)
-	$(CC) $(LIBS_PATH) -lgfortran -o  $@ $^
+	$(CC) $(LIBS_PATH) -lgfortran -lm -o  $@ $^
 
 %.o: %.c
 	$(CC) -c $(CFLAGS) -o $@ $<
